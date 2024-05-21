fa7fc4ae9a78443352d1a8d1d2a0cfc3e300088e
yukigesho <yukigesho@proton.me>
2024-05-20 22:46:43 -0700

    Add amount of users on the website right now

--- server/api/router.ts
+++ server/api/router.ts
@@ -5,11 +5,14 @@ import getAuctionData, { getLiveAuctionData } from '../getAuctionData';
 import { AuctionData } from '../api/types';
 import { liveVitals } from './vitals';
 import { observable } from '@trpc/server/observable';
+import EventEmitter from 'events';
 
 const t = initTRPC.context().create();
 
 const router = t.router;
 const procedure = t.procedure;
+const ee = new EventEmitter().setMaxListeners(Infinity);
+let alive = 0;
 
 export const appRouter = router({
   latest: procedure
@@ -39,6 +42,22 @@ export const appRouter = router({
       return liveVitals.subscribe((data) => {
         emit.next(data);
       });
+    });
+  }),
+  online: procedure.subscription(() => {
+    return observable<number>((emit) => {
+      const onChange = () => {
+        emit.next(alive);
+      };
+
+      alive++;
+      ee.on('change', onChange);
+      ee.emit('change');
+      return () => {
+        alive--;
+        ee.off('change', onChange);
+        ee.emit('change');
+      };
     });
   }),
 });
