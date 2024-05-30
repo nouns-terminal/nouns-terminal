3674607ab96cfcce8b93d82ce4892e94c92702bb
yukigesho <yukigesho@proton.me>
2024-05-29 18:22:16 -0700

    Add getServerSideProps

--- pages/noun/[nounId].tsx
+++ pages/noun/[nounId].tsx
@@ -1,16 +1,31 @@
-import { useRouter } from 'next/router';
 import Auction from '../../components/Auction';
 import SiteHeader from '../../components/SiteHeader';
 import SiteFooter from '../../components/SiteFooter';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import getAuctionData from '../../server/getAuctionData';
+import { AuctionData } from '../../server/api/types';
 
-export default function NounAuctionPage() {
-  const router = useRouter();
-  const nounId = Number(router.query.nounId) > 0 ? Number(router.query.nounId) : 0;
+export default function NounAuctionPage({ auctionData }: { auctionData: AuctionData }) {
   return (
     <>
+      <Head>
+        <title>Noun {auctionData.auction.id}</title>
+      </Head>
       <SiteHeader />
-      <Auction auctionId={nounId} />
+      <Auction auctionId={auctionData.auction.id} />
       <SiteFooter />
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const auctionId = Number(context.query.nounId) > 0 ? Number(context.query.nounId) : 0;
+  const auctionData = await getAuctionData(auctionId);
+
+  return {
+    props: {
+      auctionData,
+    },
+  };
+};
