d1eda5f6a7c720351fde6ba1ad1300ba7fe9dc30
yukigesho <yukigesho@proton.me>
2024-09-19 17:33:43 -0700

    Improve rarity in `NounInfo`

--- components/NounInfo.tsx
+++ components/NounInfo.tsx
@@ -46,8 +46,11 @@ export default function NounInfo({
                 <tr key={`noun-stats-${index}`}>
                   <td style={{ color: 'var(--mid-text)' }}>{nounProperties[index].part}</td>
                   <td>{formatNounPropertyTitle(part.filename || '')}</td>
-                  <td style={{ color: 'var(--yellow)', textAlign: 'right' }}>
-                    {formatRarity((nounProperties[index].rarity * 100).toString())}
+                  <td style={{ textAlign: 'right' }}>
+                    {formatRarity(
+                      Math.round(1 / nounProperties[index].rarity),
+                      nounProperties[index].part,
+                    )}
                   </td>
                 </tr>
               ))}
@@ -141,25 +144,51 @@ function formatNounPropertyTitle(filename: string) {
   return result;
 }
 
-function formatRarity(number: string, prefix = '') {
-  let [a, b] = number.split('.');
-  if (!b) {
-    b = '0';
-  }
-  if (b.length > 2) {
-    b = b.substring(0, 2);
+function formatRarity(number: number, part: string) {
+  const thresholds = {
+    body: [25, 20, 15, 0],
+    accessory: [250, 150, 100, 0],
+    head: [350, 250, 150, 0],
+    glasses: [25, 20, 15, 0],
+    default: [0, 0, 0, 0],
+  };
+
+  let partThresholds = thresholds.default;
+  switch (part) {
+    case 'body':
+      partThresholds = thresholds.body;
+      break;
+    case 'glasses':
+      partThresholds = thresholds.glasses;
+      break;
+    case 'accessory':
+      partThresholds = thresholds.accessory;
+      break;
+    case 'head':
+      partThresholds = thresholds.head;
+      break;
+    default:
+      break;
   }
 
-  if (a == '0' && b == '00') {
-    prefix = '<';
-    b = '01';
-  }
+  const getBars = (num: number, thresholds: number[]) => {
+    const totalBars = 4;
+    const colors = ['#E0A636', '#E09336', '#E07336', '#E03636'];
+    const yellowBars = thresholds.filter((threshold) => num > threshold).length;
+    const bars = Array.from({ length: totalBars }, (_, i) => (
+      <svg key={i} width="1.2rem" viewBox="0 0 17 17" version="1.1" style={{ marginRight: '-9px' }}>
+        <path d="M5 17h3v-17h-3v17z" fill={`${i < yellowBars ? colors[i] : 'var(--hint-text)'}`} />
+      </svg>
+    ));
+
+    return <>{bars.reverse()}</>;
+  };
+
+  const bars = getBars(number, partThresholds);
 
   return (
     <>
-      {prefix}
-      {a}.{b}
-      {'%'}
+      <span>{bars}</span>
     </>
   );
 }
